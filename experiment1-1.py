a4f06d3f7c9984cc869b9490db7815921b404890
miannelli <miiannelli@gmail.com>
2015-10-31 03:14:12 -0400

    redesigned experiment

--- experiment1-1.py
+++ experiment1-1.py
@@ -6,19 +6,20 @@ from statistics import mean, stdev
 import tablemaker
 
 
-def trial(metric, K, N, distribution):
+def trial(trial_number, metric, K, N, distribution):
     point_cloud = point.PointCloud.generate(N, K, distribution)
     point_distances = (point.distances(point_cloud.points, metric) for point in point_cloud.points)
     ratios = [min(distances)/max(distances) for distances in point_distances]
-    result = metric, K, N, mean(ratios), stdev(ratios)
+    result = trial_number, metric, K, N, mean(ratios), stdev(ratios)
     print(result)
     return result
 
 N_Values = list(range(1, 11)) + list(range(20, 101, 10))  # The dimension of space
 K_Values = [5, 50, 500]  # The number of point values
 metrics = [Metric(ord) for ord in [1, 2, inf]]  # Different norms to use
+trials = 25
 
 distribution = (lambda: random.uniform(0, 1))  # Point Distribution
 
-results = [trial(metric, K, N, distribution) for metric in metrics for N in N_Values for K in K_Values]
-tablemaker.make_table('experiment1-1', ['Metric', 'K', 'N', 'Mean', 'StandardDeviation'], results)
+results = [trial(trial_number, metric, K, N, distribution) for metric in metrics for N in N_Values for K in K_Values for trial_number in range(trials)]
+tablemaker.make_table('experiment1-1', ['TrialNumber', 'Metric', 'K', 'N', 'Mean', 'StandardDeviation'], results)
